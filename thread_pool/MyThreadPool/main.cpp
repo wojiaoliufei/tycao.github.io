c21b9ad4d827a6ed9107df0947016c263b228439
tycao <616881845@qq.com>
2018-04-08 15:49:51 +0800

    modify MyThreadPool

--- thread_pool/MyThreadPool/main.cpp
+++ thread_pool/MyThreadPool/main.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 #include "thread.h"
 
-class CMyTask : public CTask {
+
+
+class CMyTask : public csx::CTask {
 public:
-    CMyTask(){}     /** constructor */
+    CMyTask() {}
     int Run() {
-        printf("%s\n", static_cast<char*>(this->m_ptrData));
-        Sleep(5000);
+        printf("%s\n", static_cast<char*>(this->m_ptrName));
+        Sleep(3000);
         return 0;
     }
 };
 
-
 int main() {
-    /** 创建任务对象，之后塞入CThreadPool::m_vecTaskList里 */
+    /** 创建任务对象，塞进线程池中的任务队列中，供线程循环取出并执行 */
     CMyTask taskObj;
-
     char szTmp[] = "this is the first thread running!";
     taskObj.SetData(static_cast<void*>(szTmp));
 
-    /** 创建线程池，包含10个线程 */
-    CThreadPool threadPool;
+    /** 创建一个线程池，默认包含10条线程 */
+    csx::CThreadPool threadPool(10);
 
-    /** 将20个任务放进线程池中的任务池（任务列表）中 */
+    /** 将20个任务放进线程池中的任务池中 */
     for (int i = 0; i != 20; ++i)
         threadPool.AddTask(&taskObj);
 
-    while (1) {
-        printf("there are still %d tasks need to be handled.\n", threadPool.GetTaskSize());
-        if (threadPool.GetTaskSize() == 0) { // 当任务池中没有任何任务之后，所有线程都会处于pthread_cond_wait状态，即“睡眠状态”
-            if (threadPool.StopAll() == -1) {   // 当执行StopAll()之后，所有的线程都会（从睡眠状态）被唤醒，然后被销毁
+    while (true) {
+        printf("there are still %d tasks need to be handled\n", threadPool.GetTaskSize());
+        if (threadPool.GetTaskSize() == 0)
+            if (threadPool.StopAll() == -1) {
                 printf("Now i will exit from main.\n");
                 exit(0);
             }
-        }
         Sleep(2000);
     }
     return 0;
--- thread_pool/MyThreadPool/thread.cpp
+++ thread_pool/MyThreadPool/thread.cpp
@@ -1,89 +1,90 @@
-#include <cstdio>   // for printf
 #include "thread.h"
+
+namespace csx {
+
 
 
 void CTask::SetData(void* data) {
-    m_ptrData = data;
+    m_ptrName = data;
 }
 
-// static成员变量必须在类外初始化。（并且类外不能加上static）
-vector<CTask*> CThreadPool::m_vecTaskList;         //任务列表
-bool CThreadPool::shutdown = false;
+/** static 成员变量必须在类外初始化，且类外不能继续加上static */
+vector<CTask*>   CThreadPool::m_vecTaskList;                                 /** 任务池（任务队列） */
+bool             CThreadPool::shutdown = false;                              /** 线程退出标志 */
+pthread_mutex_t  CThreadPool::m_pthreadMutex = PTHREAD_MUTEX_INITIALIZER;    /** 线程同步锁 */
+pthread_cond_t   CThreadPool::m_pthreadCond = PTHREAD_COND_INITIALIZER;      /** 线程同步的条件变量 */
 
-pthread_mutex_t CThreadPool::m_pthreadMutex = PTHREAD_MUTEX_INITIALIZER;  /** 线程同步锁 */
-pthread_cond_t CThreadPool::m_pthreadCond = PTHREAD_COND_INITIALIZER;    /** 线程同步的条件变量 */
-
-// 线程池构造函数 ： 创建（固定数量的）线程
-CThreadPool::CThreadPool(int threadNum) {
-    this->m_iThreadNum = threadNum;
-    printf("I will create %d threads.\n", threadNum);
+/** 构造函数：创建（固定数量的）线程 */
+CThreadPool::CThreadPool(int threadNum)
+    : m_iThreadNum(threadNum)
+{
+    printf("i will create %d threads\n", m_iThreadNum);
     Create();
 }
 
-// 创建线程池中的线程
-int CThreadPool::Create() {
-    this->pthread_id  = static_cast<pthread_t*>(malloc(sizeof(pthread_t) * m_iThreadNum));
-    for (int i = 0; i != m_iThreadNum; ++i)
+/** 创建线程池中线程 */
+int CThreadPool::Create(void)
+{
+    pthread_id = static_cast<pthread_t*>(malloc(sizeof(pthread_t) * m_iThreadNum));
+    for (int i = 0; i != m_iThreadNum;  ++i)
         pthread_create(&pthread_id[i], nullptr, ThreadFunc, nullptr);
     return 0;
 }
-// 线程回调函数
+
+/** 线程回调函数 */
 void* CThreadPool::ThreadFunc(void* threadData) {
     pthread_t tid = pthread_self(); // 获取当前线程的线程id
     while (true) {
-        pthread_mutex_lock(&m_pthreadMutex);    /** 锁住线程同步锁 */
+        pthread_mutex_lock(&m_pthreadMutex);
 
-        /** 若任务池里的所有任务全部都处理完成了（即线程池中的任务池没有任何任务了）, 则线程处于睡眠状态*/
-        while (m_vecTaskList.size() == 0 && !shutdown) {
+        /** 若任务池中没有任何任务之后，则线程处于睡眠状态 */
+        if (m_vecTaskList.size() == 0 && !shutdown)
             pthread_cond_wait(&m_pthreadCond, &m_pthreadMutex);
-        }
 
         if (shutdown) {
             pthread_mutex_unlock(&m_pthreadMutex);
-            printf("thread %lu will exit\n", tid);
+            printf("tid %lu will exit\n", tid);
             pthread_exit(nullptr);
         }
 
         printf("tid %lu run\n", tid);
-
-        /** 取出一个任务，并处理之*/
+        /** 取出一个任务，并执行 */
         vector<CTask*>::iterator iter = m_vecTaskList.begin();
-        CTask* task = nullptr;
+        CTask* task;
         if (iter != m_vecTaskList.end()) {
             task = *iter;
             m_vecTaskList.erase(iter);
         }
-        pthread_mutex_unlock(&m_pthreadMutex);  /** 解锁线程同步锁 */
+        pthread_mutex_unlock(&m_pthreadMutex);
 
         task->Run();    /** 执行任务 */
-        printf("tid : %lu idle\n", tid);
+        printf("tid %lu idle\n", tid);
     }
     return 0;
 }
-
-// 往任务列表里面添加任务，并发出同步线程同步信号
+/** 把任务添加到任务队列中 */
 int CThreadPool::AddTask(CTask* task) {
     pthread_mutex_lock(&m_pthreadMutex);
-    this->m_vecTaskList.push_back(task);
+    m_vecTaskList.push_back(task);
     pthread_mutex_unlock(&m_pthreadMutex);
     pthread_cond_signal(&m_pthreadCond);
     return 0;
 }
-
-// 停止所有线程
+/** 使线程池中的线程退出 */
 int CThreadPool::StopAll() {
     /** 避免重复调用 */
-    if (shutdown)
-        return -1;
-    printf("Now i will end all threads!\n");
-    /** 唤醒所有（处于pthread_cond_wait状态的）线程， 线程池要销毁了 */
+    if (shutdown) return -1;
+
+    printf("i will end all threads.\n");
+    /** 唤醒所有（处于pthread_cond_wait状态）线程，线程池要销毁啦 */
     shutdown = true;
     pthread_cond_broadcast(&m_pthreadCond);
 
-    /** 阻塞等待线程退出， 否则就成僵尸了 */
+    /** 使阻塞等待线程退出，否则就成僵尸线程了 */
     for (int i = 0; i != m_iThreadNum; ++i)
         pthread_join(pthread_id[i], nullptr);
 
+    /** 释放线程池 */
     free(pthread_id);
     pthread_id = nullptr;
 
@@ -93,11 +94,10 @@ int CThreadPool::StopAll() {
 
     return 0;
 }
-
-/**
- * 获取当前队列中任务数
- */
-int CThreadPool::GetTaskSize()
-{
+/** 获取当前任务列表中的任务数量 */
+int CThreadPool::GetTaskSize() {
     return m_vecTaskList.size();
+}
+
+
 }
